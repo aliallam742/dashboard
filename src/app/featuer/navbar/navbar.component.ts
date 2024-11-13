67c1f849d3ec414b73dc935351abf1bd2189c0cd
aliallam742 <aliallam7472@gmail.com>
2024-11-13 07:46:34 +0200

    create navbar

--- src/app/featuer/navbar/navbar.component.ts
+++ src/app/featuer/navbar/navbar.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, signal } from '@angular/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MatButtonToggleModule, MatCheckboxModule, CalendarModule, CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  hideSingleSelectionIndicator = signal(true);
+  date2: string = ''
 }
