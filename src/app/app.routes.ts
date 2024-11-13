8e5e18dd2d5da29f39470841788ae3b27c3501a0
aliallam742 <aliallam7472@gmail.com>
2024-11-13 08:58:58 +0200

    create mainlayout dashbord and fixed section and handel routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     { path: '', redirectTo: "mainlayout", pathMatch: 'full' },
-    { path: 'mainlayout', loadComponent: () => import('./featuer/mainlayout/mainlayout.component').then((c) => c.MainlayoutComponent) }
+    {
+        path: 'mainlayout', loadComponent: () => import('./featuer/mainlayout/mainlayout.component').then((c) => c.MainlayoutComponent), children: [
+            { path: '', redirectTo: 'graph', pathMatch: 'full' },
+            { path: 'graph', loadComponent: () => import('./featuer/mainlayout/components/graph/graph.component').then(c => c.GraphComponent) },
+            { path: 'list', loadComponent: () => import('./featuer/mainlayout/components/list/list.component').then(c => c.ListComponent) }
+        ]
+    }
 ];
--- src/app/featuer/mainlayout/mainlayout.component.ts
+++ src/app/featuer/mainlayout/mainlayout.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-mainlayout',
   standalone: true,
-  imports: [],
+  imports: [MatCheckboxModule, MatButtonToggleModule, RouterOutlet, RouterLink, RouterModule],
   templateUrl: './mainlayout.component.html',
   styleUrl: './mainlayout.component.css'
 })
 export class MainlayoutComponent {
 
+  cardData: any = [
+    { mony: '71,028', currency: 'EGP', valueText: 'Total Value', icon: 'fa-solid fa-caret-down', number: '2', supSym: '', symbols: '%', isSelected: true, sup: '', isUp: false },
+    { mony: '20,000', currency: 'm', valueText: 'Total Sales', icon: 'fa-solid fa-caret-up', number: '50', supSym: '2', symbols: 'm', isSelected: false, sup: '2', isUp: true },
+    { mony: '71,028', currency: 'm', valueText: 'Avg Sales', icon: 'fa-solid fa-caret-down', number: '50', supSym: '2', symbols: 'm', isSelected: false, sup: '2', isUp: false },
+    { mony: '1567', currency: '', valueText: 'Total Transaction Count', icon: 'fa-solid fa-caret-up', number: '20', supSym: '', symbols: '', isSelected: false, sup: '', isUp: true }
+  ]
 }
