1adccbb1a18ed555580362a8b43c4e36d135e3b0
aliallam742 <aliallam7472@gmail.com>
2024-11-15 10:37:24 +0200

    ali change

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,10 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter, RouterModule } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),
+  importProvidersFrom(RouterModule.forRoot(routes, { useHash: true }))
+  ]
 };
